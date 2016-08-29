77ee9f81da51fc95ad1fdf4b4320f709c1351ddf
Nikolas Engelhard <engelhard@magazino.eu>
2016-08-29 12:44:10 +0200

    New StringProperty 'Display Namespace' that allows to distinguish multiple instances. It allows to create a namespace that contains both the trigger service and also the published image topic.

--- include/rviz_camera_stream/camera_display.h
+++ include/rviz_camera_stream/camera_display.h
@@ -31,6 +31,7 @@
 #define RVIZ_CAMERA_STREAM_CAMERA_DISPLAY_H
 
 #include <QObject>
+#include <string>
 
 #ifndef Q_MOC_RUN
 #include <OgreMaterial.h>
@@ -104,8 +105,12 @@ private Q_SLOTS:
   void updateTopic();
   virtual void updateQueueSize();
   virtual void updateFrameRate();
+  virtual void updateDisplayNamespace();
 
 private:
+  std::string camera_trigger_name_;
+  ros::NodeHandle nh_;
+
   void subscribe();
   void unsubscribe();
 
@@ -113,6 +118,8 @@ private:
   bool triggerCallback(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res);
   bool trigger_activated_;
   ros::Time last_image_publication_time_;
+
+  std::string findUnusedDisplayName();
 
   void caminfoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg);
 
@@ -127,6 +134,8 @@ private:
   RosTopicProperty* camera_info_property_;
   DisplayGroupVisibilityProperty* visibility_property_;
   IntProperty* queue_size_property_;
+  StringProperty* namespace_property_;
+
   FloatProperty* frame_rate_property_;
 
   sensor_msgs::CameraInfo::ConstPtr current_caminfo_;
